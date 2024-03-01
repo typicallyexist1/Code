3ccc0e467a073aea8022520039eaed98c502e862
typicallyexist1 <161828447+typicallyexist1@users.noreply.github.com>
2024-03-01 16:28:00 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">CHANDU</div>
+          <div className="right">
+            <p>MG Road Captial Road, Vijayawada</p>
+            <p>Open: 12:00 PM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CHANDU</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserve.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
